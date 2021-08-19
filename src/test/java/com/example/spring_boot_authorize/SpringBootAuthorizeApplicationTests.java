dc67698935b5719ff01266233bfb107bb5178bc7
SergSedoy <78666743+SergSedoy@users.noreply.github.com>
2021-08-19 17:10:21 +0300

    del update

--- src/test/java/com/example/spring_boot_authorize/SpringBootAuthorizeApplicationTests.java
+++ src/test/java/com/example/spring_boot_authorize/SpringBootAuthorizeApplicationTests.java
@@ -4,25 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class DemoApplicationTests {
-    @Autowired
-    TestRestTemplate restTemplate;
-	
-    private static GenericContainer<?> app = new GenericContainer<>("app:1.0");
-    private static GenericContainer<?> appLatest = new GenericContainer<>("app:latest"); 
+@SpringBootTest
+class SpringBootAuthorizeApplicationTests {
 
-    @BeforeAll
-    public static void setUp() {
-        app.start();
-	appLatest.start();
-	    
-    }
-
-    @Test
-    void contextLoads() {
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + app.getMappedPort(8080), String.class);
-        System.out.println(forEntity.getBody());
-    }
-
+	@Test
+	void contextLoads() {
+	}
 }
